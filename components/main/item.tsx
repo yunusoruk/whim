f882648b103adbf9b2254a4269ae597b93d24c81
yunusoruk <orukyunus@gmail.com>
2023-10-23 14:39:31 +0300

    fix: editor

--- components/main/item.tsx
+++ components/main/item.tsx
@@ -148,7 +148,7 @@ export const Item = ({
                         >
                             <div
                                 role="button"
-                                className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+                                className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:hover:bg-primary/10 dark:hover:bg-primary/10"
                             >
                                 <MoreHorizontal className="h-4 w-4 text-muted-foreground" />
                             </div>
@@ -172,7 +172,7 @@ export const Item = ({
                     <div
                         role="button"
                         onClick={onCreate}
-                        className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+                        className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-primary/10 dark:hover:bg-primary/10"
                     >
                         <Plus className="h-4 w-4 text-muted-foreground" />
                     </div>
